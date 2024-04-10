22c649d7fd2ad2ba0fa64a0b84f4a4521d9640c1
Pedro Ricciardi <riccip@outlook.com>
2024-04-10 10:10:06 +0100

    added movie id verification

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 const PORT = 3000
 
-const movies = [
+const moviesData = [
   {
     id: 1,
     title: "The Godfather",
@@ -27,13 +27,29 @@ const movies = [
   },
 ];
 
+const movieBiggestId = moviesData.reduce((acc, movie) => {
+  if (movie.id > acc) {
+    return movie.id;
+  }
+  return acc;
+}, 0 )
+
 app.get('/', (req, res) => {
-  res.send('Welcome to my Movies API! Endpoints available: /movies')
+  res.send('Welcome to my Movies API! Endpoints available: /movies, /movies/:movieId')
 })
 
 app.get("/movies", (req, res) => {
-  res.send(movies)
+  res.send(moviesData)
 })
+
+app.get("/movies/:movieId", (req, res) => {
+  const idToFind = Number(req.params.movieId);
+  const movie = moviesData.find((movie) => movie.id === idToFind);
+  if (!movie) {
+    return res.status(404).send(`Movie not found. Please, select a value smaller or equals than ${movieBiggestId}.`);
+  }
+  res.status(200).send(movie);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
