53db5bcfb79c09a14dbaa0d474c1ddb9ca8e9fed
Pedro Ricciardi <riccip@outlook.com>
2024-04-10 10:29:00 +0100

    Added more details if the movie id is not found

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import express from 'express'
 const app = express()
 const PORT = 3000
 
+app.use(express.json())
+
 const moviesData = [
   {
     id: 1,
@@ -27,13 +29,6 @@ const moviesData = [
   },
 ];
 
-const movieBiggestId = moviesData.reduce((acc, movie) => {
-  if (movie.id > acc) {
-    return movie.id;
-  }
-  return acc;
-}, 0 )
-
 app.get('/', (req, res) => {
   res.send('Welcome to my Movies API! Endpoints available: /movies, /movies/:movieId')
 })
@@ -43,12 +38,20 @@ app.get("/movies", (req, res) => {
 })
 
 app.get("/movies/:movieId", (req, res) => {
+  const moviesIdList = moviesData.map(movie => movie.id).join(", ");
+
   const idToFind = Number(req.params.movieId);
-  const movie = moviesData.find((movie) => movie.id === idToFind);
+  const movie = moviesData.find((movie) => Number(movie.id) === idToFind);
   if (!movie) {
-    return res.status(404).send(`Movie not found. Please, select a value smaller or equals than ${movieBiggestId}.`);
+    return res.status(404).send(`Movie not found. Please, select an ID in the list below.<br>IDs: ${moviesIdList}`);
   }
   res.status(200).send(movie);
+});
+
+app.post("/movies", (req, res) => {
+  const newMovie = req.body;
+  moviesData.push(newMovie);
+  res.send(`movie added successfully!`);
 });
 
 app.listen(PORT, () => {
